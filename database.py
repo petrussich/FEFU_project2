8afc30948c7c235b3fd572488b6f7fff68b336ef
petrussich <120343075+petrussich@users.noreply.github.com>
2023-04-25 17:52:43 +1000

    Update database.py

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ class Database:
         self.path_to_db = path_to_db
         self.create_table_of_rooms()
         self.create_table_of_users()
+        self.create_table_of_notification()
 
 
     @property
@@ -18,7 +19,7 @@ class Database:
     def execute(self, sql: str, parameters: tuple = None, fetchone=False, fetchall=False, commit=False):
         if not parameters:
             parameters = tuple()
-
+                
         connection = self.connection
         # connection.set_trace_callback(logger)
         cursor = connection.cursor()
@@ -121,7 +122,122 @@ class Database:
             #     'server_answer':'Список комнат',
             #     'rooms': rooms
             # })
+            
+    def reserve_room(self, room_number, login):
+        sql = "SELECT occupied FROM rooms WHERE room_number = ?"
+        result = self.execute(sql, (room_number,), fetchone=True)
 
+        if result == False:
+            sql_update = "UPDATE rooms SET occupied = True WHERE room_number = ?"
+            self.execute(sql_update, (result.room_number,), commit=True)
+            cur_datetime = datetime.datetime.now()
+            insert = """INSERT INTO notification
+                                VALUES (?,?,?,?)"""
+            self.execute(insert, (login, cur_datetime, 'Комната успешно зарезервирована', False), commit=True)
+
+            return json.dumps({
+                'server_answer': 'Комната успешно зарезервирована',
+                'reserve_status': 'ok',
+                'answer_status': 'ok'
+            })
+        else:
+
+            return json.dumps({
+                'server_answer': 'Комната уже занята',
+                'reserve_status': 'ok',
+                'answer_status': 'ok'
+            })
+
+    def get_notifications(self, login):
+        sql = "SELECT time, text, read_status FROM notification WHERE login = ?"
+        result = self.execute(sql, fetchall=True)
+        if result is None:
+            return json.dumps({
+                'server_answer': 'Нет новых уведомлений',
+                'answer_status': 'ok'
+            })
+        else:
+            notification = []
+
+            for item in result:
+                notification.append({
+                    'time': item[1],
+                    'text': item[2],
+                    'read_status': item[3],
+                })
+            sql_update = "UPDATE notification SET read_status = True WHERE login = ?"
+            self.execute(sql_update, (login,), commit=True)
+
+            return json.dumps({
+                'server_answer': 'Есть новые уведомления',
+                'answer_status': 'ok'
+            })
+
+    def get_all_users(self):
+        sql = "SELECT * FROM users"
+        result = self.execute(sql, fetchall=True)
+
+        users = []
+
+        for item in result:
+            users.append({
+                'user_id': item[0],
+                'first_name': item[1],
+                'last_name': item[2],
+                'room_resident': item[3],
+                'login': item[4],
+                'password': item[5],
+                'login_status': item[6],
+                'admin_status': bool(item[7])
+            })
+
+        return json.dumps({
+            'server_answer': 'Список пользователей',
+            'answer_status': 'ok'
+        })
+
+    def change_user_residence_status(self, change_type, room_resident, room_number, login): #логин не админа
+
+        if change_type == 'kick from room':
+
+            sql_update = "UPDATE rooms SET occupied = False, room_resident = '' WHERE room_number = ?"
+            self.execute(sql_update, (room_number,), commit=True)
+            cur_datetime = datetime.datetime.now()
+            insert = """INSERT INTO notification
+                                       VALUES (?,?,?,?)"""
+            self.execute(insert, (login, cur_datetime, 'Вы выселены', False), commit=True)
+            
+           return json.dumps({
+                'server_answer': 'Вы выселили пользователя',
+                'answer_status': 'ok'
+            })
+    
+        elif change_type == 'confirm reserve':
+            sql_update = "UPDATE rooms SET occupied = True, room_resident = ? WHERE room_number = ?"
+            self.execute(sql_update, (room_resident, room_number,), commit=True)
+            cur_datetime = datetime.datetime.now()
+            insert = """INSERT INTO notification
+                                        VALUES (?,?,?,?)"""
+            self.execute(insert, (login, cur_datetime, 'Вы заселены', False), commit=True)
+            
+            return json.dumps({
+                'server_answer': 'Вы заселили пользователя',
+                'answer_status': 'ok'
+            })
+
+        elif change_type == 'cansel reserve':
+            sql_update = "UPDATE rooms SET occupied = False WHERE room_number = ?"
+            self.execute(sql_update, (room_number,), commit=True)
+            cur_datetime = datetime.datetime.now()
+            insert = """INSERT INTO notification
+                                        VALUES (?,?,?,?)"""
+            self.execute(insert, (login, cur_datetime, 'Ваша резервация отменена', False), commit=True)
+            
+            return json.dumps({
+                'server_answer': 'Вы отменили резервацию пользователя',
+                'answer_status': 'ok'
+            })
+            
 if __name__ == '__main__':
     db = Database()
     db.user_register("zxc", "123", "Gleb", "Kim")
